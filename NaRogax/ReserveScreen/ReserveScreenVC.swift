7f7471b3ee5200dcc1d23f20567bc99cebdfdfd3
EgorHr <egor.hristoforov@mail.ru>
2019-03-12 11:42:09 +0300

    Data validation

--- NaRogax/ReserveScreen/ReserveScreenVC.swift
+++ NaRogax/ReserveScreen/ReserveScreenVC.swift
@@ -33,12 +33,23 @@ class ReserveScreenVC: UIViewController {
         super.viewDidLoad()
         
         ReserveBtn.layer.cornerRadius = 20
-        setStyleForTextField(field: NameField)
-        setStyleForTextField(field: PhoneField)
-        setStyleForTextField(field: EmailField)
+        setStyleForTextField(field: NameField, placeholder: "Имя")
+        setStyleForTextField(field: PhoneField, placeholder: "Телефон")
+        setStyleForTextField(field: EmailField, placeholder: "E-mail")
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        NameField.autocapitalizationType = .words
     }
     
-    func setStyleForTextField(field: UITextField!){
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func setStyleForTextField(field: UITextField!, placeholder: String){
+        field.attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                             attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.6196078431, green: 0.6196078431, blue: 0.6196078431, alpha: 1)])
         field.layer.masksToBounds = false
         field.layer.shadowRadius = 0.0
         field.layer.shadowOpacity = 1.0
@@ -46,5 +57,100 @@ class ReserveScreenVC: UIViewController {
         field.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
         field.layer.borderWidth = 0.0
     }
-
+    
+    
+    @IBAction func changeNameFieldText(_ sender: UITextField) {
+        if let name = NameField.text {
+            if name != "" {
+                NameLabel.isHidden = false
+            } else {
+                NameLabel.isHidden = true
+            }
+        }
+    }
+    
+    @IBAction func changePhoneFieldText(_ sender: UITextField) {
+        if var phone = PhoneField.text {
+            if phone != "" {
+                PhoneLabel.isHidden = false
+                if phone.prefix(1) == "+" {
+                    phone = phoneNumberLimit(num: phone, maxlen: 11)
+                    PhoneField.text = phone
+                    if !validatePhone(number: phone) {
+                        PhoneImage.isHidden = false
+                        PhoneErrorLabel.isHidden = false
+                    } else {
+                        PhoneImage.isHidden = true
+                        PhoneErrorLabel.isHidden = true
+                    }
+                } else  {
+                    phone = phoneNumberLimit(num: phone, maxlen: 10)
+                    PhoneField.text = phone
+                    if !validatePhone(number: phone) {
+                        PhoneImage.isHidden = false
+                        PhoneErrorLabel.isHidden = false
+                    } else {
+                        PhoneImage.isHidden = true
+                        PhoneErrorLabel.isHidden = true
+                    }
+                }
+            } else {
+                PhoneImage.isHidden = true
+                PhoneErrorLabel.isHidden = true
+                PhoneLabel.isHidden = true
+            }
+        }
+    }
+    
+    @IBAction func changeEmailFieldText(_ sender: UITextField) {
+        if let email = EmailField.text {
+            if email != "" {
+                EmailLabel.isHidden = false
+                if !validateEmail(email: email) {
+                    EmailErrorLabel.isHidden = false
+                    EmailImage.isHidden = false
+                } else {
+                    EmailErrorLabel.isHidden = true
+                    EmailImage.isHidden = true
+                }
+            } else {
+                EmailLabel.isHidden = true
+            }
+        }
+    }
+    
+    func phoneNumberLimit(num: String, maxlen: Int) -> String {
+        if num.count > maxlen {
+            
+            let start = num.startIndex
+            let ind = num.index(start, offsetBy: maxlen)
+            let tail = num.index(after: ind)
+            let str = num[start..<tail]
+            
+            return String(str)
+        }
+        return num
+    }
+    
+    func validatePhone(number: String) -> Bool {
+        let range = NSRange(location: 0, length: number.count)
+        let reg = "^(\\+7|8)[0-9]{10}"
+        let regex = try! NSRegularExpression(pattern: reg)
+        if regex.firstMatch(in: number, options: [], range: range) != nil{
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func validateEmail(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.count)
+        let reg = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let regex = try! NSRegularExpression(pattern: reg)
+        if regex.firstMatch(in: email, options: [], range: range) != nil{
+            return true
+        } else {
+            return false
+        }
+    }
 }
