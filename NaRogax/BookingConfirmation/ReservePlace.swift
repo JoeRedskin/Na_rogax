aa17fb437844f1e73852de9274a4184122a9f6f2
Zlobrynya <zlobrynya@gmail.com>
2019-03-12 12:01:26 +0300

    Добавил обращение к серверу, проверка доступности Создан тестовое спори, для проверки правильности открытия экранов

---
+++ NaRogax/BookingConfirmation/ReservePlace.swift
@@ -0,0 +1,29 @@
+//
+//  PostReservePlace.swift
+//  NaRogax
+//
+//  Created by User on 12/03/2019.
+//  Copyright © 2019 Zappa. All rights reserved.
+//
+
+import Foundation
+
+struct PostReservePlace: Codable {
+    var email: String
+    var name: String
+    var phone: String
+    var date: String
+    var time_from: String
+    var time_to: String
+    var table_id: Int
+}
+
+struct ResponseReservePlace: Decodable {
+    var code: Int
+    var desc: String
+    
+    enum CodingKeys: String, CodingKey{
+        case code = "code"
+        case desc = "desc"
+    }
+}
