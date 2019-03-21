52d091115ffc2fc9b944e86ad0385d9322039503
JoeRedskin <36441911+JoeRedskin@users.noreply.github.com>
2019-03-21 23:15:43 +0300

    change menu

--- NaRogax/MenuScreen/FullDishDescription/RecDishCVC.swift
+++ NaRogax/MenuScreen/FullDishDescription/RecDishCVC.swift
@@ -14,16 +14,6 @@ class RecDishCVC: UICollectionViewCell {
     @IBOutlet weak var NameLabel: UILabel!
     @IBOutlet weak var WeightLabel: UILabel!
     @IBOutlet weak var PriceLabel: UILabel!
-    @IBOutlet weak var AddToCartBtn: UIButton!
-    @IBOutlet weak var MinusStepper: UIButton!
-    @IBOutlet weak var PlusStepper: UIButton!
-    @IBOutlet weak var CountLabel: UILabel!
-    
-    var AddCount: Int = 1 {
-        didSet {
-            CountLabel.text = String(AddCount)
-        }
-    }
     
     func displayDish(dish: DishDescription){
         
@@ -56,18 +46,6 @@ class RecDishCVC: UICollectionViewCell {
             fetchImage(url_img: dish.photo)
             //Image.image = UIImage(named: "no_image")
         }
-        //AddToCartBtn.layer.borderWidth = 1
-        //AddToCartBtn.layer.borderColor = #colorLiteral(red: 1, green: 0.1098039216, blue: 0.1647058824, alpha: 1)
-        AddToCartBtn.layer.cornerRadius = 12
-        
-        PlusStepper.layer.cornerRadius = 2
-        PlusStepper.layer.borderWidth = 1
-        PlusStepper.layer.borderColor = #colorLiteral(red: 1, green: 0.1098039216, blue: 0.1647058824, alpha: 1)
-        
-        MinusStepper.layer.cornerRadius = 2
-        MinusStepper.layer.borderWidth = 1
-        MinusStepper.layer.borderColor = #colorLiteral(red: 1, green: 0.1098039216, blue: 0.1647058824, alpha: 1)
-        
     }
     
     func fetchImage(url_img: String) {
@@ -83,37 +61,5 @@ class RecDishCVC: UICollectionViewCell {
                 }
             }
         }
-    }
-    
-    func changeState(active: Bool){
-        AddToCartBtn.isHidden = !active
-        MinusStepper.isHidden = active
-        PlusStepper.isHidden = active
-        CountLabel.isHidden = active
-    }
-    
-    @IBAction func onAddToCartTap(_ sender: UIButton) {
-        changeState(active: false)
-    }
-    
-    @IBAction func decCount(_ sender: UIButton) {
-        if AddCount == 1 {
-            changeState(active: true)
-            AddCount = 1
-        } else {
-            AddCount -= 1
-        }
-        if AddCount == 14 {
-            PlusStepper.layer.backgroundColor = #colorLiteral(red: 1, green: 0.1098039216, blue: 0.1647058824, alpha: 1)
-        }
-    }
-    
-    @IBAction func incCount(_ sender: UIButton) {
-        if AddCount < 15 {
-            AddCount += 1
-            if AddCount == 15 {
-               PlusStepper.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
-            }
-        } 
     }
 }
