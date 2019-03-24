83ae2067e8f744fc3dacb5e4594ef5137e4e06a7
egorhristoforov <egor.hristoforov@mail.ru>
2019-03-24 18:49:22 +0300

    extensions file

---
+++ NaRogax/SupportFiles/Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  Extensions.swift
+//  NaRogax
+//
+//  Created by Egor on 24/03/2019.
+//  Copyright © 2019 Zappa. All rights reserved.
+//
+
+import UIKit
+
+private var __maxLengths = [UITextField: Int]()
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    @objc func fix(textField: UITextField) {
+        let t = textField.text
+        textField.text = t?.safelyLimitedTo(length: maxLength)
+    }
+}
+
+extension String
+{
+    func safelyLimitedTo(length n: Int)->String {
+        if (self.count <= n) {
+            return self
+        }
+        return String( Array(self).prefix(upTo: n) )
+    }
+}
+
+extension UITextField {
+    
+    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return action == #selector(UIResponderStandardEditActions.cut) || action == #selector(UIResponderStandardEditActions.copy)
+    }
+}
