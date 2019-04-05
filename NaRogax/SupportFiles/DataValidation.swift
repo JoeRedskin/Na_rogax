7107cce027761549cd5c2a7a4a49de1a9f8206e4
egorhristoforov <egor.hristoforov@mail.ru>
2019-04-05 22:25:19 +0300

    Bugfix
    Bug #1, Bug #2

--- NaRogax/SupportFiles/DataValidation.swift
+++ NaRogax/SupportFiles/DataValidation.swift
@@ -11,7 +11,7 @@ import Foundation
 func validateName(name: String) -> Bool {
     if name.count > 1 {
         let range = NSRange(location: 0, length: name.count)
-        let reg = "^[А-Яа-яЁё]{1,30}$"
+        let reg = "^[a-zA-ZА-Яа-яЁё]{1,30}$"
         let regex = try! NSRegularExpression(pattern: reg)
         if regex.firstMatch(in: name, options: [], range: range) != nil{
             return true
@@ -36,7 +36,7 @@ func validatePassword(pass: String) -> Bool {
 
 func validateEmail(email: String) -> Bool {
     let range = NSRange(location: 0, length: email.count)
-    let reg = "^[a-zA-Z]{1,2}[A-Za-z0-9._-]{0,62}[@]{1}[A-Za-z]{2}[A-Za-z0-9.-]{0,8}[.]{1}[A-Za-z]{2,255}"
+    let reg = "^[a-zA-Z]{1,2}[A-Za-z0-9._-]{0,62}[@]{1}[A-Za-z]{2}[A-Za-z0-9.-]{0,8}[.]{1}[A-Za-z]{2,255}$"
     let regex = try! NSRegularExpression(pattern: reg)
     if regex.firstMatch(in: email, options: [], range: range) != nil{
         return true
